992f3c7ff118b67b62c1a2c337e877a378504f21
Caleb Lugo <cnl1211999@gmail.com>
2023-07-26 17:44:43 -0700

    Added scriptable objects for all cards

--- Assets/Scripts/ScriptableObjects/PlayingCardSO.cs
+++ Assets/Scripts/ScriptableObjects/PlayingCardSO.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Scriptable object for a card in One Poker
+/// The only relevant information required in One Poker is the card rank,
+/// so the card suit, and color will be omitted since it is not relevant to the game.
+/// </summary>
 [CreateAssetMenu()]
 public class PlayingCardSO : ScriptableObject
 {
+    // Ranks of cards represented by an enum
     public enum Rank
     {        
         Two,
@@ -22,11 +28,6 @@ public class PlayingCardSO : ScriptableObject
         Ace,
     }
 
-    /// <summary>
-    /// Scriptable object for a card in One Poker
-    /// The only relevant information required in One Poker is the card rank,
-    /// so the card suit, and color will be omitted since it is not relevant to the game.
-    /// </summary>
     public Transform cardTransform;   
     public Rank cardRank;
     public string cardName;
