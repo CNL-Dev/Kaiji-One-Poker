b208ef80f3e9e540e656f89b941477ffd6fe9698
Caleb Lugo <cnl1211999@gmail.com>
2023-07-27 15:01:29 -0700

    Added opponent script

---
+++ Assets/Scripts/Opponent.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opponent : MonoBehaviour, IPlaying
+{
+    public void DrawCard()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public PlayingCardSO PlayCard()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveCard(int index)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
