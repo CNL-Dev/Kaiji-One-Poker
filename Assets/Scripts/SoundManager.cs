c8097e00cf0a9f454cb48891149854c710545459
Caleb Lugo <cnl1211999@gmail.com>
2023-09-08 22:38:05 -0700

    Added ChangeVolume method

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    // const string for the playerprefs volume
+    private const string PLAYER_PREFS_SOUND_EFFECTS_VOLUME = "SoundEffectsVolume";
+
     // Singleton
     public static SoundManager Instance { get; private set; }
 
@@ -12,6 +15,26 @@ public class SoundManager : MonoBehaviour
     private void Awake()
     {
         Instance = this; 
+    }
+
+    // Adjust the volume by either increasing or decreasing it.
+    public void ChangeVolume(bool increased)
+    {
+        // If volume is being increased, raise it by 0.1f, if not then lower it
+        // by the same amount. Ignore the request if the volume is either muted
+        // (at 0.0f) or maxed out (at 1f).
+        if (increased && volume < 1f)
+        {
+            volume += 0.1f;
+        }
+        else if(volume > 0f)
+        {
+            volume -= 0.1f;
+        }
+
+        // Save the player prefs for volume
+        PlayerPrefs.SetFloat(PLAYER_PREFS_SOUND_EFFECTS_VOLUME, volume);
+        PlayerPrefs.Save();
     }
 
     // Plays a audio clip within the world space.
