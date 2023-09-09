fd35fe34d61b5175fcda44ad8a06f212b4023fa7
Caleb Lugo <cnl1211999@gmail.com>
2023-09-08 22:28:50 -0700

    Added PlaySound method to SoundManager

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -7,8 +7,16 @@ public class SoundManager : MonoBehaviour
     // Singleton
     public static SoundManager Instance { get; private set; }
 
+    private float volume = 1f;
+
     private void Awake()
     {
         Instance = this; 
+    }
+
+    // Plays a audio clip within the world space.
+    private void PlaySound(AudioClip audioClip, Vector3 position, float volumeMultiplier = 1f)
+    {
+        AudioSource.PlayClipAtPoint(audioClip, position, volumeMultiplier * volume);
     }
 }
